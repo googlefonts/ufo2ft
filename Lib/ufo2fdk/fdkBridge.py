350cc0de596a7b95867296806e4ee6de752b5aa8
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2013-04-24 13:58:04 +0000

    Patch from Ben Kiel that fixes a situation in which checkOutlines would not be run even if it was asked to. Thanks, Ben!

--- Lib/ufo2fdk/fdkBridge.py
+++ Lib/ufo2fdk/fdkBridge.py
@@ -143,6 +143,11 @@ def checkOutlines(fontPath, removeOverlap=True, correctContourDirection=True):
         stderr, stdout = _execute(c)
         allStderr.append(stderr)
         allStdout.append(stdout)
+    if not removeOverlap and not correctContourDirection:
+        c = cmds + ["-O", fontPath]
+        stderr, stdout = _execute(c)
+        allStderr.append(stderr)
+        allStdout.append(stdout)
     return "\n".join(allStderr), "\n".join(allStdout)
 
 outlineCheckFirstLineRE = re.compile(
