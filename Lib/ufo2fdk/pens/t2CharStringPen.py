3c943dd68c0bd6f9d3f35de71699374e615044ae
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2013-07-09 17:48:54 +0000

    Do not store rmoveto unless it is followed by a rlineto or rcurveto command. This prevents anchors from showing up in the charstrings.
    This could be done upstream in the RelativeCoordinatePen, but I'm not sure what ramifications that would have elsewhere.

--- Lib/ufo2fdk/pens/t2CharStringPen.py
+++ Lib/ufo2fdk/pens/t2CharStringPen.py
@@ -6,6 +6,7 @@ class T2CharStringPen(RelativeCoordinatePen):
 
     def __init__(self, width, glyphSet):
         RelativeCoordinatePen.__init__(self, glyphSet)
+        self._heldMove = None
         self._program = []
         if width is not None:
             self._program.append(roundInt(width))
@@ -13,14 +14,21 @@ class T2CharStringPen(RelativeCoordinatePen):
     def _relativeMoveTo(self, pt):
         pt = roundIntPoint(pt)
         x, y = pt
-        self._program.extend([x, y, "rmoveto"])
+        self._heldMove = [x, y, "rmoveto"]
+
+    def _storeHeldMove(self):
+        if self._heldMove is None:
+            self._program.extend(self._heldMove)
+            self._heldMove = None
 
     def _relativeLineTo(self, pt):
+        self._storeHeldMove()
         pt = roundIntPoint(pt)
         x, y = pt
         self._program.extend([x, y, "rlineto"])
 
     def _relativeCurveToOne(self, pt1, pt2, pt3):
+        self._storeHeldMove()
         pt1 = roundIntPoint(pt1)
         pt2 = roundIntPoint(pt2)
         pt3 = roundIntPoint(pt3)
