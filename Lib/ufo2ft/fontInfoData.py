87b270fa4ed23fc60e8eb1cc1c0660cf5faf357d
James Godfrey-Kittle <jamesgk@google.com>
2016-02-17 15:58:43 -0800

    Pull upstream changes from ufo2fdk
    Incorporates the following commits from ufo2fdk:
    1d5cdc6a98688fbf60b8db37ad9e9c4d44686204
    8713490e761026c6ec663dcfabc74112c288f2f6

--- Lib/ufo2ft/fontInfoData.py
+++ Lib/ufo2ft/fontInfoData.py
@@ -144,18 +144,23 @@ def openTypeOS2WinAscentFallback(info):
     """
     Fallback to the maximum y value of the font's bounding box.
     If that is not available, fallback to *ascender*.
+    if the maximum y value is negative, fallback to 0 (zero).
     """
     font = info.getParent()
     if font is None:
-        return getAttrWithFallback(info, "ascender")
-    bounds = getFontBounds(font)
-    xMin, yMin, xMax, yMax = bounds
+        yMax = getAttrWithFallback(info, "ascender")
+    else:
+        bounds = getFontBounds(font)
+        xMin, yMin, xMax, yMax = bounds
+    if yMax < 0:
+        return 0
     return yMax
 
 def openTypeOS2WinDescentFallback(info):
     """
     Fallback to the minimum y value of the font's bounding box.
     If that is not available, fallback to *descender*.
+    If the minimum y value is positive, fallback to 0 (zero).
     """
     font = info.getParent()
     if font is None:
@@ -164,6 +169,8 @@ def openTypeOS2WinDescentFallback(info):
     if bounds is None:
         return abs(getAttrWithFallback(info, "descender"))
     xMin, yMin, xMax, yMax = bounds
+    if yMin > 0:
+        return 0
     return abs(yMin)
 
 # postscript
