754ff3bf3d9ddddecc07af1c4af0d966983dcff8
James Godfrey-Kittle <jamesgk@google.com>
2016-04-28 13:05:57 -0700

    Clearer variable names in __init__

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -6,22 +6,22 @@ from ufo2ft.markFeatureWriter import MarkFeatureWriter
 from ufo2ft.outlineOTF import OutlineOTFCompiler, OutlineTTFCompiler
 
 
-def compileOTF(font, glyphOrder=None, outlineCompilerClass=OutlineOTFCompiler,
+def compileOTF(ufo, glyphOrder=None, outlineCompilerClass=OutlineOTFCompiler,
                featureCompilerClass=FeatureOTFCompiler, mtiFeaFiles=None,
                kernWriter=KernFeatureWriter, markWriter=MarkFeatureWriter):
     """Create FontTools CFF font from a UFO."""
 
-    outlineCompiler = outlineCompilerClass(font, glyphOrder=glyphOrder)
-    outline = outlineCompiler.compile()
+    outlineCompiler = outlineCompilerClass(ufo, glyphOrder=glyphOrder)
+    otf = outlineCompiler.compile()
 
     featureCompiler = featureCompilerClass(
-        font, outline, kernWriter, markWriter, mtiFeaFiles=mtiFeaFiles)
+        ufo, otf, kernWriter, markWriter, mtiFeaFiles=mtiFeaFiles)
     featureCompiler.compile()
 
-    return outline
+    return otf
 
 
-def compileTTF(font, outlineCompilerClass=OutlineTTFCompiler, **kwargs):
+def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler, **kwargs):
     """Create FontTools TrueType font from a UFO."""
 
-    return compileOTF(font, outlineCompilerClass=outlineCompilerClass, **kwargs)
+    return compileOTF(ufo, outlineCompilerClass=outlineCompilerClass, **kwargs)
