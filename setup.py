2c9e1e0e348db102749c6a287e7db241b615f52e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-11-24 10:18:08 +0000

    setup.py: version 0.2.0, update metadata, add install_requires, etc.

--- setup.py
+++ setup.py
@@ -1,28 +1,56 @@
 #!/usr/bin/env python
 
 from __future__ import print_function, division, absolute_import
-from setuptools import setup
-
-try:
-    import fontTools
-except:
-    print("*** Warning: ufo2ft requires FontTools, see:")
-    print("    fonttools.sf.net")
-
-try:
-    import ufoLib
-except:
-    print("*** Warning: ufo2ft requires ufoLib, see:")
-    print("    https://github.com/unified-font-object/ufoLib")
+import sys
+from setuptools import setup, find_packages
 
 
-setup(name="ufo2ft",
-    version="0.1",
-    description="A bridge between UFOs and FontTools.",
-    author="Tal Leming",
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="ufo2ft",
+    version="0.2.0",
+    author="Tal Leming, James Godfrey-Kittle",
     author_email="tal@typesupply.com",
-    url="http://code.typesupply.com",
+    maintainer="James Godfrey-Kittle",
+    maintainer_email="jamesgk@google.com",
+    description="A bridge between UFOs and FontTools.",
+    long_description=long_description,
+    url="https://github.com/googlei18n/ufo2ft",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
     license="MIT",
-    packages=["ufo2ft"],
-    package_dir={"":"Lib"}
+    setup_requires=pytest_runner + wheel,
+    tests_require=[
+        'pytest>=2.8',
+    ],
+    install_requires=[
+        "fonttools>=3.1.2",
+        "ufoLib>=2.0.0",
+        "defcon>=0.2.0",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
