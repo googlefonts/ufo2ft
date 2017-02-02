06e97d50b9a6df57f886d81f54a8af233b7a2f35
Antonio Cavedoni <antonio@cavedoni.org>
2017-02-02 15:50:40 +0100

    More robust edge case testing.

--- tests/glyphOrder_test.py
+++ tests/glyphOrder_test.py
@@ -12,16 +12,32 @@ def getTestUFO():
 
 class TestGlyphOrder(unittest.TestCase):
 
-    NEW_GLYPH_ORDER = ['.notdef', 'space', 'b', 'a', 'c']
-
     def setUp(self):
         self.ufo = getTestUFO()
-        self.ufo.lib['public.glyphOrder'] = self.NEW_GLYPH_ORDER
 
-    def test_compile_glyph_order(self):
+    def test_compile_original_glyph_order(self):
+        DEFAULT_ORDER = ['.notdef', 'space', 'a', 'b', 'c']
         compiler = OutlineTTFCompiler(self.ufo)
         compiler.compile()
-        self.assertEqual(compiler.otf.getGlyphOrder(), self.NEW_GLYPH_ORDER)
+        self.assertEqual(compiler.otf.getGlyphOrder(), DEFAULT_ORDER)
+
+    def test_compile_tweaked_glyph_order(self):
+        NEW_ORDER = ['.notdef', 'space', 'b', 'a', 'c']
+        self.ufo.lib['public.glyphOrder'] = NEW_ORDER
+        compiler = OutlineTTFCompiler(self.ufo)
+        compiler.compile()
+        self.assertEqual(compiler.otf.getGlyphOrder(), NEW_ORDER)
+
+    def test_compile_strange_glyph_order(self):
+        """Move space and .notdef to end of glyph ids
+        ufo2ft always puts .notdef first.
+        """
+        NEW_ORDER = ['b', 'a', 'c', 'space', '.notdef']
+        EXPECTED_ORDER = ['.notdef', 'b', 'a', 'c', 'space']
+        self.ufo.lib['public.glyphOrder'] = NEW_ORDER
+        compiler = OutlineTTFCompiler(self.ufo)
+        compiler.compile()
+        self.assertEqual(compiler.otf.getGlyphOrder(), EXPECTED_ORDER)
 
 
 if __name__ == "__main__":
