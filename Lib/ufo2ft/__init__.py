311ae370cd7fa3cf9e17c6cf9ca3b65b1feb37a1
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-23 16:59:20 +0000

    Release 0.4.1

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -7,7 +7,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "0.4.1.dev0"
+__version__ = "0.4.1"
 
 
 def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
