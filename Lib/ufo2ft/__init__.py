72209b92771f7034cab92d75381b1c877f402d4e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-04 19:33:09 +0100

    Bump version: 0.4.2 → 0.4.3.dev0

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -7,7 +7,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "0.4.2"
+__version__ = "0.4.3.dev0"
 
 
 def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
