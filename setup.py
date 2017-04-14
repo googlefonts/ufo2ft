5130895ce32c31f3df3baf08f087611f81a2b389
Sascha Brawer <sascha@brawer.ch>
2017-04-14 18:48:43 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="ufo2ft",
-    version="0.5.0.dev0",
+    version="0.5.0",
     author="Tal Leming, James Godfrey-Kittle",
     author_email="tal@typesupply.com",
     maintainer="James Godfrey-Kittle",
