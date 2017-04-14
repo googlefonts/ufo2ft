cf22dde3540cf821b3172cdaf348020e540f4361
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 08:34:21 +0100

    Release 0.4.3
    test

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -11,7 +11,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "0.4.3.dev0"
+__version__ = "0.4.3"
 log = logging.getLogger(__name__)
 
 
