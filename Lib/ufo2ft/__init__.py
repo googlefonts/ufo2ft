9bd8b61b05ac9d817652c9d872c5ac824a6050c4
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 08:53:38 +0100

    Bump version: 0.4.3 → 0.5.0.dev0

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -11,7 +11,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "0.4.3"
+__version__ = "0.5.0.dev0"
 log = logging.getLogger(__name__)
 
 
