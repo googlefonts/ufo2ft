c6f570b0d6360ac1387ffaea89f4cd9365bf31c9
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 08:53:38 +0100

    Bump version: 0.4.3 → 0.5.0.dev0

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="ufo2ft",
-    version="0.4.3",
+    version="0.5.0.dev0",
     author="Tal Leming, James Godfrey-Kittle",
     author_email="tal@typesupply.com",
     maintainer="James Godfrey-Kittle",
