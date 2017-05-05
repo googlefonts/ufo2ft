1937e078f4763c27970fc9e0fe0278d135124c82
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-05-05 15:09:38 +0100

    Release 0.5.1

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="ufo2ft",
-    version="0.5.1.dev0",
+    version="0.5.1",
     author="Tal Leming, James Godfrey-Kittle",
     author_email="tal@typesupply.com",
     maintainer="James Godfrey-Kittle",
