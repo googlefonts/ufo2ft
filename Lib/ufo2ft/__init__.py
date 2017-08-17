f4bac6b2b77b6512ee6543c788c7b384e8e499f4
Cosimo Lupo <cosimo@anthrotype.com>
2017-08-17 15:06:35 +0100

    Release 0.6.2

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -11,7 +11,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "0.6.2.dev0"
+__version__ = "0.6.2"
 log = logging.getLogger(__name__)
 
 
