5b318265d353e0193ba81f003165d1ca14ddf612
Denis Moyogo Jacquerye <denis.jacquerye@daltonmaag.com>
2017-09-14 03:24:21 +0100

    Use defaultFeatureWriters

--- Lib/ufo2ft/featureWriter/baseFeatureWriter.py
+++ Lib/ufo2ft/featureWriter/baseFeatureWriter.py
@@ -10,26 +10,10 @@ def liststr(glyphs):
 class BaseFeatureWriter(object):
     """Abstract features writer"""
 
-    def __init__(self, font, features=(), scriptLangs=()):
+    def __init__(self, font, features=()):
         self.font = font
         self.features = features
-        self._scriptLangs = scriptLangs
 
     def write(self, **kwargs):
         """Write features and class definitions"""
         raise NotImplementedError
-
-    def _get_scriptLangs(self):
-        if self._scriptLangs:
-            return self._scriptLangs
-        else:
-            return {"DFLT": ("dflt", )}
-
-    scriptLangs = property(_get_scriptLangs)
-
-    def scriptLangsLabel(self):
-        label = []
-        for script in self.scriptLangs:
-            langs = "_".join(lang for lang in self.scriptLangs[script])
-            label.append(script + "_" + langs)
-        return "_".join(label)
