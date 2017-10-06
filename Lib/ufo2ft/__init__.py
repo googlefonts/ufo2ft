4ada715e44ff7e7031fad4c554e093e7c35e3efc
Cosimo Lupo <cosimo@anthrotype.com>
2017-10-06 17:25:37 +0100

    remove unused 'mtiFeaFiles' arg, only compile embedded mtiFeatures
    This removes the ability to pass MTI feature files as side input
    to the compiler.
    Only MTI features that are embedded in the UFO sources, i.e. in the
    'data' folder, under "com.github.googlei18n.ufo2ft.mtiFeatures/%s.mti",
    are now supported.
    
    The deprecation period was long enough for a feature that nobody
    else apart from fontmake itself uses.

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -16,7 +16,7 @@ log = logging.getLogger(__name__)
 
 
 def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
-               featureCompilerClass=FeatureCompiler, mtiFeaFiles=None,
+               featureCompilerClass=FeatureCompiler,
                kernWriterClass=KernFeatureWriter, markWriterClass=MarkFeatureWriter,
                glyphOrder=None, useProductionNames=True, optimizeCFF=True,
                roundTolerance=None):
@@ -38,7 +38,7 @@ def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
 
     featureCompiler = featureCompilerClass(
         ufo, otf, kernWriterClass=kernWriterClass, markWriterClass=markWriterClass,
-        mtiFeatures=_getMtiFeatures(ufo, mtiFeaFiles))
+        mtiFeatures=_getMtiFeatures(ufo))
     featureCompiler.compile()
 
     postProcessor = PostProcessor(otf, ufo)
@@ -48,7 +48,7 @@ def compileOTF(ufo, outlineCompilerClass=OutlineOTFCompiler,
 
 
 def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler,
-               featureCompilerClass=FeatureCompiler, mtiFeaFiles=None,
+               featureCompilerClass=FeatureCompiler,
                kernWriterClass=KernFeatureWriter, markWriterClass=MarkFeatureWriter,
                glyphOrder=None, useProductionNames=True,
                convertCubics=True, cubicConversionError=None):
@@ -63,7 +63,7 @@ def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler,
 
     featureCompiler = featureCompilerClass(
         ufo, otf, kernWriterClass=kernWriterClass, markWriterClass=markWriterClass,
-        mtiFeatures=_getMtiFeatures(ufo, mtiFeaFiles))
+        mtiFeatures=_getMtiFeatures(ufo))
     featureCompiler.compile()
 
     postProcessor = PostProcessor(otf, ufo)
@@ -72,26 +72,11 @@ def compileTTF(ufo, outlineCompilerClass=OutlineTTFCompiler,
     return otf
 
 
-def _getMtiFeatures(ufo, mtiFeaFiles=None):
+def _getMtiFeatures(ufo):
     features = {}
     prefix = "com.github.googlei18n.ufo2ft.mtiFeatures/"
     for fileName in ufo.data.fileNames:
         if fileName.startswith(prefix) and fileName.endswith(".mti"):
             content = tounicode(ufo.data[fileName], encoding="utf-8")
             features[fileName[len(prefix):-4]] = content
-
-    # TODO: Remove the ability to pass MTI feature files as side input
-    # to the compiler. https://github.com/googlei18n/fontmake/issues/289
-    if mtiFeaFiles is not None:
-        log.warn("Future versions of this font compiler will remove "
-                 "the capability to inject MTI features as side input. "
-                 "Instead, please embed your MTI feature files into the "
-                 "compiled UFO sources. See "
-                 "https://github.com/googlei18n/fontmake/issues/289.")
-        # For now, the side input wins over any MTI features inside the UFO.
-        features = {}
-        for table, feapath in mtiFeaFiles.items():
-            with open(feapath, "rt", encoding="utf-8") as feafile:
-                features[table] = feafile.read()
-
     return features if len(features) > 0 else None
