552a9533111ff949977c586617c65a8d1cdfd628
Cosimo Lupo <cosimo@anthrotype.com>
2017-10-25 19:18:47 +0100

    [cubic2Quadratic] dump stats

--- Lib/ufo2ft/filters/cubicToQuadratic.py
+++ Lib/ufo2ft/filters/cubicToQuadratic.py
@@ -5,6 +5,11 @@ from ufo2ft.filters import BaseFilter
 from cu2qu.ufo import DEFAULT_MAX_ERR
 from cu2qu.pens import Cu2QuPointPen
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class CubicToQuadraticFilter(BaseFilter):
 
@@ -17,6 +22,18 @@ class CubicToQuadraticFilter(BaseFilter):
     def start(self):
         relativeError = self.conversionError or DEFAULT_MAX_ERR
         self.absoluteError = relativeError * self.unitsPerEm
+        self.stats = None
+
+    def __call__(self, glyphSet):
+        self.stats = {}
+        if super(CubicToQuadraticFilter, self).__call__(glyphSet):
+            self.dump_stats()
+
+    def dump_stats(self):
+        stats = self.stats
+        logger.info('New spline lengths: %s' % (', '.join(
+                    '%s: %d' % (l, stats[l]) for l in sorted(stats.keys()))))
+        self.stats = None
 
     def filter(self, glyph, glyphSet=None):
         if not len(glyph):
@@ -25,7 +42,8 @@ class CubicToQuadraticFilter(BaseFilter):
         pen = Cu2QuPointPen(
             glyph.getPointPen(),
             self.absoluteError,
-            reverse_direction=self.reverseDirection)
+            reverse_direction=self.reverseDirection,
+            stats=self.stats)
         contours = list(glyph)
         glyph.clearContours()
         for contour in contours:
