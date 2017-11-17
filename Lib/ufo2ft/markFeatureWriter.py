fa8bf61891ff5e3a94d14d71c226bc06ec069cd5
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-17 10:51:28 +0000

    [coverage] ignore module aliases
    we don't import them in the tests

--- Lib/ufo2ft/markFeatureWriter.py
+++ Lib/ufo2ft/markFeatureWriter.py
@@ -1,4 +1,4 @@
 """This module is deprecated! It's kept here only for backward compatibility.
 Please import the new ufo2ft.featureWriters module.
 """
-from ufo2ft.featureWriters.markFeatureWriter import *
+from ufo2ft.featureWriters.markFeatureWriter import *  # pragma: no cover
