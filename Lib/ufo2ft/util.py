61d3ba54b415bf604426e5f6bf2913308fba335d
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-21 16:51:33 +0100

    [markFeatureWriter] ensure pos rule order is determistic
    when one does `for glyph in font`, the `__iter__` method yields
    glyph objects in random order because font.keys() is an unsorted
    collection in defcon.
    
    Thus, when we are building the mark feature and collecting anchors
    we have to iterate over glyphs in a specific order, i.e. the
    font.glyphOrder if it's present, else alphabetic order.

---
+++ Lib/ufo2ft/util.py
@@ -0,0 +1,26 @@
+from __future__ import print_function, division, absolute_import
+
+
+def makeOfficialGlyphOrder(font, glyphOrder=None):
+    """ Make the final glyph order for 'font'.
+
+    If glyphOrder is None, try getting the font.glyphOrder list.
+    If not explicit glyphOrder is defined, sort glyphs alphabetically.
+
+    If ".notdef" glyph is present in the font, force this to always be
+    the first glyph (at index 0).
+    """
+    if glyphOrder is None:
+        glyphOrder = getattr(font, "glyphOrder", ())
+    orderedGlyphs = [".notdef"]
+    for glyphName in glyphOrder:
+        if glyphName == ".notdef":
+            continue
+        if glyphName not in font:
+            continue
+        orderedGlyphs.append(glyphName)
+    orderedGlyphSet = set(orderedGlyphs)
+    for glyphName in sorted(font.keys()):
+        if glyphName not in orderedGlyphSet:
+            orderedGlyphs.append(glyphName)
+    return orderedGlyphs
