googlefonts/ufo2ft

commit 51e09107171af562dba7ae31786bc652def1aa1b

[featureCompiler] featureWriters=None means 'use default writers'
This makes it easier for clients to optionally override the default,

Lib/ufo2ft/__init__.py
@@ -5,7 +5,6 @@ from fontTools.misc.py23 import *
 from ufo2ft.preProcessor import (
     OTFPreProcessor, TTFPreProcessor, TTFInterpolatablePreProcessor)
 from ufo2ft.featureCompiler import FeatureCompiler
-from ufo2ft.featureWriters import DEFAULT_FEATURE_WRITERS
 from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
@@ -18,7 +17,7 @@ def compileOTF(ufo, preProcessorClass=OTFPreProcessor,
                featureCompilerClass=FeatureCompiler,
                kernWriterClass=None,  # deprecated
                markWriterClass=None,  # deprecated
-               featureWriters=DEFAULT_FEATURE_WRITERS,
+               featureWriters=None,
                glyphOrder=None,
                useProductionNames=True,
                optimizeCFF=True,
@@ -69,7 +68,7 @@ def compileTTF(ufo, preProcessorClass=TTFPreProcessor,
                featureCompilerClass=FeatureCompiler,
                kernWriterClass=None,  # deprecated
                markWriterClass=None,  # deprecated
-               featureWriters=DEFAULT_FEATURE_WRITERS,
+               featureWriters=None,
                glyphOrder=None,
                useProductionNames=True,
                convertCubics=True,
@@ -113,7 +112,7 @@ def compileInterpolatableTTFs(ufos,
                               preProcessorClass=TTFInterpolatablePreProcessor,
                               outlineCompilerClass=OutlineTTFCompiler,
                               featureCompilerClass=FeatureCompiler,
-                              featureWriters=DEFAULT_FEATURE_WRITERS,
+                              featureWriters=None,
                               glyphOrder=None,
                               useProductionNames=True,
                               cubicConversionError=None,

Lib/ufo2ft/featureCompiler.py
@@ -32,10 +32,12 @@ class FeatureCompiler(object):
     """
 
     def __init__(self, font, outline,
-                 featureWriters=DEFAULT_FEATURE_WRITERS,
+                 featureWriters=None,
                  mtiFeatures=None):
         self.font = font
         self.outline = outline
+        if featureWriters is None:
+            featureWriters = DEFAULT_FEATURE_WRITERS
         self.featureWriters = []
         for writer in featureWriters:
             if isclass(writer):

commit 6e0a6452199b969c1644f8be718b6f7fd6099bc5

fix and add test for deprecated {kern,mark}WriterClass args
A previos commit 51e0910 broke them.
This ensure that the old and the new featureWriters args cannot
be used at the same time, and that the old still work as before
but with a deprecation warning.

(Need to upadate fontmake to use the new argument)

Lib/ufo2ft/__init__.py
@@ -51,7 +51,7 @@ def compileOTF(ufo, preProcessorClass=OTFPreProcessor,
         roundTolerance=roundTolerance)
     otf = outlineCompiler.compile()
 
-    _replaceDeprecatedFeatureWriters(
+    featureWriters = _replaceDeprecatedFeatureWriters(
         featureWriters, kernWriterClass, markWriterClass)
     featureCompiler = featureCompilerClass(
         ufo, otf, featureWriters=featureWriters,
@@ -96,7 +96,7 @@ def compileTTF(ufo, preProcessorClass=TTFPreProcessor,
         ufo, glyphSet=glyphSet, glyphOrder=glyphOrder)
     otf = outlineCompiler.compile()
 
-    _replaceDeprecatedFeatureWriters(
+    featureWriters = _replaceDeprecatedFeatureWriters(
         featureWriters, kernWriterClass, markWriterClass)
     featureCompiler = featureCompilerClass(
         ufo, otf, featureWriters=featureWriters,
@@ -166,14 +166,29 @@ def _deprecateArgument(arg, repl):
 def _replaceDeprecatedFeatureWriters(featureWriters,
                                      kernWriterClass=None,
                                      markWriterClass=None):
+    if not any([kernWriterClass, markWriterClass]):
+        return featureWriters
+
+    if featureWriters is not None:
+        raise TypeError(
+            "the new 'featureWriters' argument, and the old (deprecated) "
+            "'kernWriterClass' and 'markWriterClass' arguments are mutually "
+            "exclusive.")
+
+    featureWriters = []
+
     if kernWriterClass is not None:
         _deprecateArgument("kernWriterClass", "featureWriters")
-        for i, writer in enumerate(featureWriters):
-            if "kern" in writer.features:
-                featureWriters[i] = kernWriterClass
+        featureWriters.append(kernWriterClass)
+    else:
+        from ufo2ft.featureWriters import KernFeatureWriter
+        featureWriters.append(KernFeatureWriter)
 
     if markWriterClass is not None:
         _deprecateArgument("markWriterClass", "featureWriters")
-        for i, writer in enumerate(featureWriters):
-            if "mark" in writer.features:
-                featureWriters[i] = markWriterClass
+        featureWriters.append(markWriterClass)
+    else:
+        from ufo2ft.featureWriters import MarkFeatureWriter
+        featureWriters.append(MarkFeatureWriter)
+
+    return featureWriters

tests/compiler_test.py
@@ -3,6 +3,8 @@ from __future__ import \
 from fontTools.misc.py23 import *
 from defcon import Font
 from ufo2ft import compileOTF, compileTTF, compileInterpolatableTTFs
+from ufo2ft.featureWriters import KernFeatureWriter, MarkFeatureWriter
+import warnings
 import difflib
 import os
 import sys
@@ -32,6 +34,35 @@ class CompilerTest(unittest.TestCase):
 
     def test_TestFont_CFF(self):
         self.expectTTX(compileOTF(loadUFO("TestFont.ufo")), "TestFont-CFF.ttx")
+
+    def test_deprecated_arguments(self):
+        ufo = loadUFO("TestFont.ufo")
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always", UserWarning)
+
+            compileTTF(ufo, kernWriterClass=KernFeatureWriter)
+
+            self.assertEqual(len(w), 1)
+            self.assertEqual(w[-1].category, UserWarning)
+            self.assertIn(
+                "'kernWriterClass' is deprecated; use 'featureWriters'",
+                str(w[-1].message))
+
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always", UserWarning)
+
+            compileOTF(ufo, markWriterClass=MarkFeatureWriter)
+
+            self.assertEqual(len(w), 1)
+            self.assertEqual(w[-1].category, UserWarning)
+            self.assertIn(
+                "'markWriterClass' is deprecated; use 'featureWriters'",
+                str(w[-1].message))
+
+        with self.assertRaises(TypeError):
+            compileTTF(ufo, kernWriterClass=KernFeatureWriter,
+                       featureWriters=[MarkFeatureWriter])
 
     def test_features(self):
         """Checks how the compiler handles features.fea