6610b99ac53ccf6dc7b0f0b5ad6e80606356149f
Cosimo Lupo <cosimo@anthrotype.com>
2017-12-18 13:45:42 +0000

    Bump version: 1.1.0 → 1.1.1.dev0

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -11,7 +11,7 @@ from ufo2ft.outlineCompiler import OutlineOTFCompiler, OutlineTTFCompiler
 from ufo2ft.postProcessor import PostProcessor
 
 
-__version__ = "1.1.0"
+__version__ = "1.1.1.dev0"
 
 
 def compileOTF(ufo, preProcessorClass=OTFPreProcessor,
