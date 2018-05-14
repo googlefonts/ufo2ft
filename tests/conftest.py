6c5478f5983278a4d7ddedb7d1c08e12ee214ed1
Cosimo Lupo <cosimo@anthrotype.com>
2018-05-14 16:53:10 +0100

    conftest: parametrize InfoClass as well

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,15 @@ import pytest
 
 
 @pytest.fixture(scope="session", params=["defcon", "ufoLib2"])
-def FontClass(request):
-    module = importlib.import_module(request.param)
-    return module.Font
+def ufo_module(request):
+    return importlib.import_module(request.param)
+
+
+@pytest.fixture(scope="session")
+def FontClass(ufo_module):
+    return ufo_module.Font
+
+
+@pytest.fixture(scope="session")
+def InfoClass(ufo_module):
+    return ufo_module.objects.info.Info
