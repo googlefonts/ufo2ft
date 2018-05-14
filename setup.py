efbd8f9b352b60ada3df5a4a94282bc7a3e4451f
Cosimo Lupo <cosimo@anthrotype.com>
2018-05-14 12:23:44 +0100

    bump cu2qu and defcon requirements

--- setup.py
+++ setup.py
@@ -168,8 +168,8 @@ setup(
     install_requires=[
         "fonttools>=3.23.0",
         "ufoLib>=2.1.0",
-        "defcon>=0.3.4",
-        "cu2qu>=1.2.0",
+        "defcon>=0.4.0",
+        "cu2qu>=1.5.0",
         "compreffor>=0.4.5",
         "booleanOperations>=0.7.1",
     ],
