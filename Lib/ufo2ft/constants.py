c7459a8f90d186b733ae643991968cca009c7ae0
Cosimo Lupo <cosimo@anthrotype.com>
2018-06-06 17:06:53 +0100

    Add constants.py module
    we should probably move other constan values scattered elsewhere here..

---
+++ Lib/ufo2ft/constants.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+
+
+UFO2FT_PREFIX = 'com.github.googlei18n.ufo2ft.'
+GLYPHS_PREFIX = 'com.schriftgestaltung.'
+
+USE_PRODUCTION_NAMES = UFO2FT_PREFIX + "useProductionNames"
+GLYPHS_DONT_USE_PRODUCTION_NAMES = GLYPHS_PREFIX + "Don't use Production Names"
