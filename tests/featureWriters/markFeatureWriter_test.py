bb99e1abe0fec4b254e6151d9e85c820d4e1dfcc
Denis Moyogo Jacquerye <moyogo@gmail.com>
2021-02-16 14:15:13 +0000

    Don't support InsertMarkerEnd

--- tests/featureWriters/markFeatureWriter_test.py
+++ tests/featureWriters/markFeatureWriter_test.py
@@ -472,66 +472,6 @@ class MarkFeatureWriterTest(FeatureWriterTest):
         ):
             writer.write(testufo, feaFile)
 
-    def test_update_feature(self, testufo):
-        testufo.newGlyph("e")
-        writer = MarkFeatureWriter(
-            mode="append",
-            insertFeatureMarker="#> feature",
-            insertFeatureMarkerEnd="#< feature",
-        )
-        testufo.features.text = dedent(
-            """\
-            markClass acutecomb <anchor 100 200> @MC_top;
-            markClass tildecomb <anchor 100 200> @MC_top;
-
-            feature mark {
-                #> feature
-                #
-                #< feature
-
-            } mark;
-
-            feature mkmk {
-                #> feature
-                #
-                #< feature
-
-            } mkmk;
-            """
-        )
-        feaFile = parseLayoutFeatures(testufo)
-        assert writer.write(testufo, feaFile)
-        assert str(feaFile) == dedent(
-            """\
-            markClass acutecomb <anchor 100 200> @MC_top;
-            markClass tildecomb <anchor 100 200> @MC_top;
-            feature mark {
-                #> feature
-                lookup mark2base {
-                    pos base a <anchor 100 200> mark @MC_top;
-                } mark2base;
-
-                lookup mark2liga {
-                    pos ligature f_i <anchor 100 500> mark @MC_top
-                        ligComponent <anchor 600 500> mark @MC_top;
-                } mark2liga;
-
-                #< feature
-            } mark;
-
-            feature mkmk {
-                #> feature
-                lookup mark2mark_top {
-                    @MFS_mark2mark_top = [acutecomb tildecomb];
-                    lookupflag UseMarkFilteringSet @MFS_mark2mark_top;
-                    pos mark tildecomb <anchor 100 300> mark @MC_top;
-                } mark2mark_top;
-
-                #< feature
-            } mkmk;
-            """
-        )
-
     def test_mark_mkmk_features(self, testufo):
         writer = MarkFeatureWriter()  # by default both mark + mkmk are built
         feaFile = ast.FeatureFile()
