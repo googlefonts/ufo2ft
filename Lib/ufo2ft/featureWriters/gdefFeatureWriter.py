c2351a9d56ed639bd485c0b8a927eedd9dbc924a
Denis Moyogo Jacquerye <moyogo@gmail.com>
2021-03-18 13:00:27 +0000

    gdefFeatureWriter: merge _makeGDEF and _write, remove shouldContinue

--- Lib/ufo2ft/featureWriters/gdefFeatureWriter.py
+++ Lib/ufo2ft/featureWriters/gdefFeatureWriter.py
@@ -22,33 +22,27 @@ class GdefFeatureWriter(BaseFeatureWriter):
         if ctx.gdefTableBlock:
             for fea in ctx.gdefTableBlock.statements:
                 if isinstance(fea, ast.GlyphClassDefStatement):
-                    ctx.todo.remove("GlyphClassDefs")
+                    ctx.todo.discard("GlyphClassDefs")
                 elif isinstance(fea, ast.LigatureCaretByIndexStatement) or isinstance(
                     fea, ast.LigatureCaretByPosStatement
                 ):
-                    ctx.todo.remove("LigatureCarets")
+                    ctx.todo.discard("LigatureCarets")
+
+                if not ctx.todo:
+                    break
 
         ctx.orderedGlyphSet = self.getOrderedGlyphSet()
 
         if "GlyphClassDefs" in ctx.todo:
             ctx.openTypeCategories = self.getOpenTypeCategories()
+            if not any(ctx.openTypeCategories):
+                ctx.todo.remove("GlyphClassDefs")
         if "LigatureCarets" in ctx.todo:
             ctx.ligatureCarets = self._getLigatureCarets()
+            if not ctx.ligatureCarets:
+                ctx.todo.remove("LigatureCarets")
 
         return ctx
-
-    def shouldContinue(self):
-        todo = self.context.todo
-        if not todo:
-            return super().shouldContinue()
-
-        if "LigatureCarets" in todo and not self.context.ligatureCarets:
-            todo.remove("LigatureCarets")
-
-        if "GlyphClassDefs" in todo and not any(self.context.openTypeCategories):
-            todo.remove("GlyphClassDefs")
-
-        return super().shouldContinue()
 
     def _getLigatureCarets(self):
         carets = dict()
@@ -77,10 +71,12 @@ class GdefFeatureWriter(BaseFeatureWriter):
     def _sortedGlyphClass(self, glyphNames):
         return sorted(n for n in self.context.orderedGlyphSet if n in glyphNames)
 
-    def _makeGDEF(self):
+    def _write(self):
+        feaFile = self.context.feaFile
         gdefTableBlock = self.context.gdefTableBlock
         if not gdefTableBlock:
             gdefTableBlock = ast.TableBlock("GDEF")
+            feaFile.statements.append(gdefTableBlock)
 
         if "GlyphClassDefs" in self.context.todo:
             categories = self.context.openTypeCategories
@@ -98,14 +94,5 @@ class GdefFeatureWriter(BaseFeatureWriter):
                 for glyphName, carets in self.context.ligatureCarets.items()
             ]
             gdefTableBlock.statements.extend(ligatureCarets)
-
-        return gdefTableBlock
-
-    def _write(self):
-        feaFile = self.context.feaFile
-        if self.context.gdefTableBlock:
-            self._makeGDEF()
-        else:
-            feaFile.statements.append(self._makeGDEF())
 
         return True
