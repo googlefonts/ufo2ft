1754c54a40390ce2e81f485f9e88d2497b40440e
Cosimo Lupo <clupo@google.com>
2021-03-19 11:04:31 +0000

    setup.py: bump minimum fonttools to 4.21.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     setup_requires=pytest_runner + wheel + ["setuptools_scm"],
     tests_require=["pytest>=2.8"],
     install_requires=[
-        "fonttools[ufo]>=4.17.1",
+        "fonttools[ufo]>=4.21.1",
         "cu2qu>=1.6.7",
         "compreffor>=0.4.6",
         "booleanOperations>=0.9.0",
