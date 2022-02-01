61ccd25a1e0046fee508e8f6e10866daa9722940
Simon Cozens <simon@simon-cozens.org>
2022-02-01 15:59:09 +0000

    Fix typo in USE_SCRIPTS list

--- Lib/ufo2ft/constants.py
+++ Lib/ufo2ft/constants.py
@@ -64,7 +64,7 @@ USE_SCRIPTS = [
     "Limb",  # Limbu
     "Tale",  # Tai Le
     "Bugi",  # Buginese
-    "khar",  # Kharosthi
+    "Khar",  # Kharosthi
     "Sylo",  # Syloti Nagri
     "tfng",  # Tifinagh
     "Bali",  # Balinese
