ae6a5845c2dc0925a678efbae38d33b3f7148c8b
Denis Moyogo Jacquerye <moyogo@gmail.com>
2022-08-26 18:56:20 +0200

    Add cvt, fpgm, prep to SPARSE_TTF_MASTER_TABLES

--- tests/outlineCompiler_test.py
+++ tests/outlineCompiler_test.py
@@ -1046,11 +1046,7 @@ def test_custom_layer_compilation_interpolatable(layertestrgufo, layertestbdufo)
         "edotabove",
     ]
 
-    sparse_tables = [
-        tag
-        for tag in master_ttfs[1].keys()
-        if tag not in ("GlyphOrder", "cvt ", "fpgm", "prep")
-    ]
+    sparse_tables = [tag for tag in master_ttfs[1].keys() if tag != "GlyphOrder"]
     assert SPARSE_TTF_MASTER_TABLES.issuperset(sparse_tables)
 
 
@@ -1079,11 +1075,7 @@ def test_custom_layer_compilation_interpolatable_from_ds(designspace, inplace):
         "edotabove",
     ]
 
-    sparse_tables = [
-        tag
-        for tag in master_ttfs[1].keys()
-        if tag not in ("GlyphOrder", "cvt ", "fpgm", "prep")
-    ]
+    sparse_tables = [tag for tag in master_ttfs[1].keys() if tag != "GlyphOrder"]
     assert SPARSE_TTF_MASTER_TABLES.issuperset(sparse_tables)
 
     # sentinel value used by varLib to ignore the post table for this sparse
