158219f84570066123f834b01ea713708020605a
Simon Cozens <simon@simon-cozens.org>
2022-11-08 14:03:17 +0000

    Report locations for default and master on failure

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -27,6 +27,7 @@ from ufo2ft.util import (
     ensure_all_sources_have_names,
     init_kwargs,
     prune_unknown_kwargs,
+    location_to_string,
 )
 
 try:
@@ -765,7 +766,19 @@ def _compileNeededSources(
                 continue
             default_source = vfDoc.findDefault()
             if default_source is None:
-                raise InvalidDesignSpaceData("No default source.")
+                default_location = location_to_string(vfDoc.newDefaultLocation())
+                master_locations = []
+                for sourceDescriptor in vfDoc.sources:
+                    master_location = sourceDescriptor.name + " at "
+                    master_location += location_to_string(
+                        sourceDescriptor.getFullDesignLocation(vfDoc)
+                    )
+                    master_locations.append(master_location)
+                master_location_descriptions = "\n".join(master_locations)
+                raise InvalidDesignSpaceData(
+                    f"No default source; expected default master at {default_location}."
+                    f" Found master locations:\n{master_location_descriptions}"
+                )
             vfNameToBaseUfo[vfName] = default_source.font
             for source in vfDoc.sources:
                 sourcesToCompile.add(source.name)
