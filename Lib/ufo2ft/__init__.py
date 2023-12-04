2242439adca62de4e13c05415f0ce47045fe1d28
Simon Cozens <simon@simon-cozens.org>
2023-12-04 13:19:51 +0000

    Don't re-export CFFOptimization (requires fontmake change)

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -4,7 +4,6 @@ from ufo2ft._compilers.otfCompiler import OTFCompiler
 from ufo2ft._compilers.ttfCompiler import TTFCompiler
 from ufo2ft._compilers.variableCFF2sCompiler import VariableCFF2sCompiler
 from ufo2ft._compilers.variableTTFsCompiler import VariableTTFsCompiler
-from ufo2ft.constants import CFFOptimization  # noqa: F401 (fontmake uses it)
 
 __all__ = [
     "compileTTF",
