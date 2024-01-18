39a5ab9c952f0f293100391fa21c370e2409f08a
Cosimo Lupo <clupo@google.com>
2024-01-18 11:35:53 +0000

    bump min cffsubr>=0.3.0

--- setup.py
+++ setup.py
@@ -29,14 +29,14 @@ setup(
     setup_requires=pytest_runner + wheel + ["setuptools_scm"],
     tests_require=["pytest>=2.8"],
     install_requires=[
-        "fonttools[ufo]>=4.46.0",
-        "cffsubr>=0.2.8",
+        "fonttools[ufo]>=4.47.2",
+        "cffsubr>=0.3.0",
         "booleanOperations>=0.9.0",
     ],
     extras_require={
-        "pathops": ["skia-pathops>=0.5.1"],
+        "pathops": ["skia-pathops>=0.8.0"],
         "cffsubr": [],  # keep empty for backward compat
-        "compreffor": ["compreffor>=0.4.6"],
+        "compreffor": ["compreffor>=0.5.5"],
     },
     python_requires=">=3.8",
     classifiers=[
