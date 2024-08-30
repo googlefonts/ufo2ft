51ffcba1b66e5134e926e373e002bb838094977e
Just van Rossum <justvanrossum@gmail.com>
2024-08-30 07:59:24 +0200

    Take discrete axes into account when checking for feature compatibility

--- Lib/ufo2ft/_compilers/baseCompiler.py
+++ Lib/ufo2ft/_compilers/baseCompiler.py
@@ -326,9 +326,9 @@ class BaseInterpolatableCompiler(BaseCompiler):
 
         # If the feature files are compatible between the sources, we can save
         # time by building a variable feature file right at the end.
-        can_optimize_features = self.variableFeatures and _featuresCompatible(
-            designSpaceDoc
-        )
+        can_optimize_features = self.variableFeatures and all(
+            _featuresCompatible(doc) for doc in interpolableSubDocs
+         )
         if can_optimize_features:
             self.logger.info("Features are compatible across masters; building later")
 
